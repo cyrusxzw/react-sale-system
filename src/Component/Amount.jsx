d866ffd9025a91c351656e2bbeb7706b5aa73423
Zengwei Xu <xuzengwei06@gmail.com>
2020-03-21 17:58:23 +1100

    add amount report

---
+++ src/Component/Amount.jsx
@@ -0,0 +1,85 @@
+import React, { PureComponent } from "react";
+import {
+  BarChart,
+  Bar,
+  Cell,
+  XAxis,
+  YAxis,
+  CartesianGrid,
+  Tooltip,
+  Legend
+} from "recharts";
+
+export default class Amount extends React.Component {
+
+  render() {
+    const { data } = this.props;
+    const newList = [];
+    const tempItems = data.map(sneaker => {
+      const { buy_time, sold_time } = sneaker;
+      const tempBuyDate = buy_time.substring(0, 7);
+      const tempSoldDate = sold_time.substring(0, 7);
+      return (sneaker = {
+        tempBuyDate,
+        tempSoldDate
+      });
+    });
+
+    const groupBy =  ( array , f ) => {
+      let groups = {};
+      array.forEach( function( o ) {
+          let group = JSON.stringify( f(o) );
+          groups[group] = groups[group] || [];
+          groups[group].push( o );
+      });
+      return Object.keys(groups).map( function( group ) {
+          return groups[group];
+      });
+    }
+
+    if(tempItems.length !==0 ) {
+      const items = tempItems;
+      let sorted = groupBy(items, function(item){
+        return [item.tempBuyDate];
+      });
+
+      //console.log(sorted);
+      sorted.forEach((e, index) => {
+        const totalPurchaseAmount = e.length;
+       
+        const filtered = e.filter((i) => {
+          const soldDate = i.tempSoldDate;
+          if(soldDate !== "") {
+            return true;
+          } 
+        })
+        const totalSoldAmount = filtered.length;
+        const detail = {
+          "month" : e[index].tempBuyDate,
+          "买入": totalPurchaseAmount,
+          "卖出": totalSoldAmount
+        }
+        newList.push(detail);
+        
+      })
+    }
+    return (
+      <BarChart
+        width={1000}
+        height={600}
+        data={newList}
+        margin={{
+          top: 5, right: 30, left: 20, bottom: 5,
+        }}
+      >
+        <CartesianGrid strokeDasharray="3 3" />
+        <XAxis dataKey="month" />
+        <YAxis />
+        <Tooltip />
+        <Legend />
+        <Bar dataKey="买入" fill="#8884d8" />
+        <Bar dataKey="卖出" fill="#82ca9d" />
+      </BarChart>
+    );
+  }
+}
--- src/Component/Report.jsx
+++ src/Component/Report.jsx
@@ -1,90 +1,113 @@
-import React from 'react';
-import { LineChart, Line, CartesianGrid, XAxis, YAxis, Tooltip } from 'recharts';
-import styled from 'styled-components';
+import React from "react";
+import {
+  LineChart,
+  Line,
+  CartesianGrid,
+  XAxis,
+  YAxis,
+  Tooltip
+} from "recharts";
+import styled from "styled-components";
+import Amount from "./Amount";
 
 const H1 = styled.h1`
-    text-align: center;
-    font-size: 2.2rem;
-    color: #1890ff;
-`
+  text-align: center;
+  font-size: 2.2rem;
+  color: #1890ff;
+`;
 
 export default class Report extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            data: []
-        }
+  constructor(props) {
+    super(props);
+    this.state = {
+      data: [],
+      show: false
+    };
+    this.showAmountForm = this.showAmountForm.bind(this);
+  }
+
+  componentDidMount() {
+    fetch("https://sneaker-system.herokuapp.com/sneakers")
+      .then(res => {
+        return res.json();
+      })
+      .then(sneakers => {
+        this.setState({
+          data: sneakers
+        });
+      });
+  }
+
+  showAmountForm() {
+    const { show } = this.state;
+    this.setState({
+      show: !show
+    });
+  }
+
+  render() {
+    const { data: sneakers } = this.state;
+    let total = "";
+    sneakers.forEach(({ sold_price, buy_price }) => {
+      const profit = sold_price - buy_price;
+      total = +total + +profit;
+    });
+
+    const tempItems = sneakers.map(sneaker => {
+      const { buy_time, sold_price, buy_price } = sneaker;
+      const 利润 = sold_price - buy_price;
+      const tempDate = buy_time.substring(0, 7);
+      return (sneaker = {
+        tempDate,
+        利润
+      });
+    });
+
+    let temp = {};
+    tempItems.forEach(tempItem => {
+      const { tempDate, 利润 } = tempItem;
+      const key = tempDate;
+      if (temp[key]) {
+        temp[key].tempDate = tempDate;
+        temp[key].利润 += 利润;
+      } else {
+        temp[key] = {};
+        temp[key].tempDate = tempDate;
+        temp[key].利润 = 利润;
+      }
+    });
+
+    const reportData = [];
+    for (let i in temp) {
+      reportData.push(temp[i]);
     }
 
-    componentDidMount() {
-        fetch("https://sneaker-system.herokuapp.com/sneakers").then((res) => {
-            return res.json();
-        }).then((sneakers) => {
-            this.setState({
-                data: sneakers
-            })
+    //console.log(reportData);
 
-        })
-    }
-
-    render() {
-        const { data: sneakers } = this.state;
-        let total = '';
-        sneakers.forEach(({ sold_price, buy_price }) => {
-            const profit = sold_price - buy_price;
-            total = +total + +profit;
-        })
-
-        const tempItems = sneakers.map((sneaker) => {
-            const { buy_time, sold_price, buy_price } = sneaker;
-            const 利润 = sold_price - buy_price;
-            const tempDate = buy_time.substring(0, 7);
-            return sneaker = {
-                tempDate,
-                利润
-            }
-        })
-
-        let temp = {};
-        tempItems.forEach((tempItem) => {
-            const { tempDate, 利润 } = tempItem;
-            const key = tempDate;
-            if(temp[key]) {
-                temp[key].tempDate = tempDate;
-                temp[key].利润 += 利润;
-            }
-            else {
-                temp[key] = {};
-                temp[key].tempDate = tempDate;
-                temp[key].利润 = 利润;
-            }
-        })
- 
-        const reportData = [];
-        for(let i in temp){
-            reportData.push(temp[i]);
-         }
-
-         //console.log(reportData);
-
-        return (
-            <div>
-                <H1 className="total-header">
-                    {`总利润：$${total}`}
-                </H1>
-                <LineChart
-                    width={1000}
-                    height={600}
-                    style={{margin:"2rem auto"}}
-                    data={reportData}
-                >
-                    <CartesianGrid strokeDasharray="3 3" />
-                    <XAxis dataKey="tempDate" />
-                    <YAxis />
-                    <Line type="monotone" dataKey="利润" stroke="#8884d8"/>
-                    <Tooltip />
-                </LineChart>
-            </div>
-        )
-    }
+    return (
+      <div className="report-container">
+        <H1 className="total-header">{`总利润：$${total}`}</H1>
+        <div className={`chart-container ${this.state.show ? "hideChart" : ""}`}>
+          <LineChart
+            width={1000}
+            height={600}
+            style={{ margin: "2rem auto" }}
+            data={reportData}>
+            <CartesianGrid strokeDasharray="3 3" />
+            <XAxis dataKey="tempDate" />
+            <YAxis />
+            <Line type="monotone" dataKey="利润" stroke="#8884d8" />
+            <Tooltip />
+          </LineChart>
+        </div>
+        <button className="check-amount" onClick={this.showAmountForm}>
+          {this.state.show ? "查看利润统计" : "查看买卖数量统计"}
+        </button>
+        <div
+          className={`amount-container ${this.state.show ? "showAmount" : ""}`}>
+          <Amount data={this.state.data}></Amount>
+        </div>
+      </div>
+    );
+  }
 }
