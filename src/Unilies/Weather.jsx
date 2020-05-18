a0bdf97f0611595240980c4b573d081c39283782
Cyrus Xu <cyrus@overdose.digital>
2020-05-18 18:49:27 +1000

    change weather api

--- src/Unilies/Weather.jsx
+++ src/Unilies/Weather.jsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Row, Col, Card } from "antd";
-import axios from './Axios';
 
 export default class Weather extends React.Component{
     constructor(props){
@@ -8,7 +7,7 @@ export default class Weather extends React.Component{
         this.state = {
             weather: "",
             temperature: "",
-            dayPictureUrl: "",
+            imgUrl: "",
             data: ""
         }
     }
@@ -18,20 +17,18 @@ export default class Weather extends React.Component{
     }
 
     getWeatherAPIData(){
-        let city = '上海';
-        axios.jsonp({
-            url:'http://api.map.baidu.com/telematics/v3/weather?location='+encodeURIComponent(city)+'&output=json&ak=3p49MVra6urFRGOT9s8UBWr2'
-        }).then((res)=>{
-            if(res.status == 'success'){
-                let data = res.results[0].weather_data[0];
-                //console.log(data)
-                this.setState({
-                    weather:data.weather, 
-                    temperature:data.temperature,
-                    dayPictureUrl: data.dayPictureUrl
-                })
-            }
+        let city = '36';
+        fetch('https://sapi.k780.com/?app=weather.today&weaid='+city+'&appkey=10003&sign=b59bc3ef6191eb9f747dd4e83c99f2a4&format=json').then((res) => {
+            return res.json();
+        }).then(({ result }) => {
+            console.log(result);
+            this.setState({
+                weather: result.weather,
+                temperature: `${result.temp_low}℃ - ${result.temp_high}℃`,
+                imgUrl: result.weather_icon
+            })
         })
+
     }
 
     getAusCovid(){
@@ -52,9 +49,6 @@ export default class Weather extends React.Component{
            confirmed = data.latest_data.confirmed;
            added = data.today.confirmed;
         }
-
-        // const { confirmed } = data.latest_data;
-        // console.log(confirmed)
         const imgStyle = {
             maxWidth: '30px',
             marginLeft: '10px',
@@ -67,7 +61,7 @@ export default class Weather extends React.Component{
                 <Col span={10}>澳洲确诊总数：{confirmed}&nbsp;&nbsp;较昨日增长：{added}</Col>
                 <Col span={8}></Col>
                 <Col span={6}>
-                  <div style={{ textAlign: "right" }}>上海天气: <img src={this.state.dayPictureUrl} style={imgStyle}/>{this.state.weather
+                  <div style={{ textAlign: "right" }}>上海天气: <img src={this.state.imgUrl} style={imgStyle}/>{this.state.weather
         }&nbsp;&nbsp;{this.state.temperature}</div>
                 </Col>
               </Row>
