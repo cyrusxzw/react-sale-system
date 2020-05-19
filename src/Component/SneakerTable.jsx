6a0dc31680e2303fa1cc4a16e93e54f1eb2aaea8
Cyrus Xu <cyrus@overdose.digital>
2020-05-20 09:13:39 +1000

    add loading

--- src/Component/SneakerTable.jsx
+++ src/Component/SneakerTable.jsx
@@ -1,14 +1,17 @@
 import React from "react";
 import MaterialTable from "material-table";
+import { Spin } from "antd";
 import axios from "axios";
 import SearchBasedOnMonth from "../Component/SearchBasedOnMonth";
 import { notification } from "antd";
 import Weather from "../Unilies/Weather";
+import "../Style/page.css";
 
 export default class SneakerTable extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
+      loading: true,
       columns: [
         {
           title: "商品",
@@ -31,7 +34,7 @@ export default class SneakerTable extends React.Component {
       ],
       data: [],
       filteredData: [],
-      clicked: false
+      clicked: false,
     };
   }
 
@@ -58,6 +61,7 @@ export default class SneakerTable extends React.Component {
         newSneakers = newSneakers.filter(({ id }) => id !== "");
         this.setState({
           data: newSneakers,
+          loading: false,
           //filteredData: newSneakers
         });
       });
@@ -65,12 +69,11 @@ export default class SneakerTable extends React.Component {
 
   onMonthClick(month) {
     const { data, filteredData } = this.state;
-    if(month === 'clear') {
+    if (month === "clear") {
       this.setState({
         filteredData: data,
       });
-    }
-    else {
+    } else {
       const sameMonthData = data.filter(
         ({ buy_time }) => parseInt(buy_time.split("-")[1]) === month + 1
       );
@@ -83,7 +86,7 @@ export default class SneakerTable extends React.Component {
       }
       this.setState({
         filteredData: sameMonthData,
-        clicked: true
+        clicked: true,
       });
     }
   }
@@ -91,219 +94,234 @@ export default class SneakerTable extends React.Component {
   render() {
     return (
       <div>
-        <Weather></Weather>
-        <SearchBasedOnMonth
-          onMonthClick={this.onMonthClick.bind(this)}></SearchBasedOnMonth>
-        <MaterialTable
-          title="统计表"
-          options={{
-            exportButton: true,
-          }}
-          columns={this.state.columns}
-          data={this.state.filteredData.length === 0 && this.state.clicked == false ? this.state.data : this.state.filteredData}
-          editable={{
-            onRowAdd: (newData) =>
-              new Promise((resolve, reject) => {
-                setTimeout(() => {
-                  {
-                    const data = Array.from(this.state.filteredData);
-                    const originalData = {
-                      name: "",
-                      size: "",
-                      buy_time: "",
-                      sold_time: "",
-                      buy_price: "",
-                      sold_price: "",
-                      buyer: "",
-                    };
-                    const sentData = {
-                      ...originalData,
-                      ...newData,
-                    };
-
-                    axios
-                      .post(
-                        "https://sneaker-system.herokuapp.com/sneakers",
-                        sentData
-                      )
-                      .then(() => {
-                        console.log(sentData);
-                        const { sold_price, buy_price } = newData;
-                        const profit = sold_price - buy_price;
-                        if (sold_price && buy_price) {
-                          newData = {
-                            ...newData,
-                            profit: profit,
-                          };
-                        }
-
-                        //console.log(newData);
-                        data.push(newData);
-
-                        this.setState({ filteredData: data }, () => resolve());
-                      })
-                      .catch(() => {
-                        alert("出错啦！快去找许增威！");
-                      });
-                  }
-                  resolve();
-                  //window.location.reload();
-                }, 1000);
-              }),
-            onRowUpdate: (newData, oldData) =>
-              new Promise((resolve, reject) => {
-                setTimeout(() => {
-                  {
-                    let data = Array.from(this.state.filteredData);
-                    const index = data.indexOf(oldData);
-                    data[index] = newData;
-                    const { id } = oldData;
-                    delete newData["profit"];
-                    let { buy_time, sold_time } = newData;
-                    if (buy_time == null || buy_time == "") {
-                      buy_time = "";
-                      newData = {
-                        ...newData,
-                        buy_time,
+        <div className={this.state.loading ? "havingLoading spinWrapper" : "spinWrapper"}>
+          <Spin
+            tip="正在加载数据..."
+            spinning={this.state.loading}
+            delay={500}></Spin>
+        </div>
+        <div>
+          <Weather></Weather>
+          <SearchBasedOnMonth
+            onMonthClick={this.onMonthClick.bind(this)}></SearchBasedOnMonth>
+          <MaterialTable
+            title="统计表"
+            options={{
+              exportButton: true,
+            }}
+            columns={this.state.columns}
+            data={
+              this.state.filteredData.length === 0 &&
+              this.state.clicked == false
+                ? this.state.data
+                : this.state.filteredData
+            }
+            editable={{
+              onRowAdd: (newData) =>
+                new Promise((resolve, reject) => {
+                  setTimeout(() => {
+                    {
+                      const data = Array.from(this.state.filteredData);
+                      const originalData = {
+                        name: "",
+                        size: "",
+                        buy_time: "",
+                        sold_time: "",
+                        buy_price: "",
+                        sold_price: "",
+                        buyer: "",
                       };
-                    }
-                    if (sold_time == null || sold_time == "") {
-                      sold_time = "";
-                      newData = {
+                      const sentData = {
+                        ...originalData,
                         ...newData,
-                        sold_time,
                       };
-                    }
-                    //console.log(buy_time);
-                    if (!id) {
-                      fetch("https://sneaker-system.herokuapp.com/sneakers")
-                        .then((res) => {
-                          return res.json();
+
+                      axios
+                        .post(
+                          "https://sneaker-system.herokuapp.com/sneakers",
+                          sentData
+                        )
+                        .then(() => {
+                          console.log(sentData);
+                          const { sold_price, buy_price } = newData;
+                          const profit = sold_price - buy_price;
+                          if (sold_price && buy_price) {
+                            newData = {
+                              ...newData,
+                              profit: profit,
+                            };
+                          }
+
+                          //console.log(newData);
+                          data.push(newData);
+
+                          this.setState({ filteredData: data }, () =>
+                            resolve()
+                          );
                         })
-                        .then((sneakers) => {
-                          const indexWithId = sneakers.indexOf(oldData);
-                          const dataWithId = sneakers.splice(indexWithId, 1);
+                        .catch(() => {
+                          alert("出错啦！快去找许增威！");
+                        });
+                    }
+                    resolve();
+                    //window.location.reload();
+                  }, 1000);
+                }),
+              onRowUpdate: (newData, oldData) =>
+                new Promise((resolve, reject) => {
+                  setTimeout(() => {
+                    {
+                      let data = Array.from(this.state.filteredData);
+                      const index = data.indexOf(oldData);
+                      data[index] = newData;
+                      const { id } = oldData;
+                      delete newData["profit"];
+                      let { buy_time, sold_time } = newData;
+                      if (buy_time == null || buy_time == "") {
+                        buy_time = "";
+                        newData = {
+                          ...newData,
+                          buy_time,
+                        };
+                      }
+                      if (sold_time == null || sold_time == "") {
+                        sold_time = "";
+                        newData = {
+                          ...newData,
+                          sold_time,
+                        };
+                      }
+                      //console.log(buy_time);
+                      if (!id) {
+                        fetch("https://sneaker-system.herokuapp.com/sneakers")
+                          .then((res) => {
+                            return res.json();
+                          })
+                          .then((sneakers) => {
+                            const indexWithId = sneakers.indexOf(oldData);
+                            const dataWithId = sneakers.splice(indexWithId, 1);
 
-                          const { id } = dataWithId[0];
-                          axios
-                            .patch(
-                              `https://sneaker-system.herokuapp.com/sneakers/${id}`,
-                              newData
-                            )
-                            .then(() => {
-                              const { buy_price, sold_price } = newData;
-                              const newProfit = sold_price - buy_price;
-                              newData = {
-                                ...newData,
-                                profit: newProfit,
-                              };
+                            const { id } = dataWithId[0];
+                            axios
+                              .patch(
+                                `https://sneaker-system.herokuapp.com/sneakers/${id}`,
+                                newData
+                              )
+                              .then(() => {
+                                const { buy_price, sold_price } = newData;
+                                const newProfit = sold_price - buy_price;
+                                newData = {
+                                  ...newData,
+                                  profit: newProfit,
+                                };
 
-                              const { id: newDataId } = newData;
-                              data = data.map((sneaker) => {
-                                const { id } = sneaker;
-                                if (id == newDataId) {
-                                  return (sneaker = newData);
-                                } else {
-                                  return sneaker;
-                                }
+                                const { id: newDataId } = newData;
+                                data = data.map((sneaker) => {
+                                  const { id } = sneaker;
+                                  if (id == newDataId) {
+                                    return (sneaker = newData);
+                                  } else {
+                                    return sneaker;
+                                  }
+                                });
+                                //console.log(dataForProfitUsing);
+                                this.setState({ filteredData: data }, () =>
+                                  resolve()
+                                );
+                              })
+                              .catch(() => {
+                                alert("出错啦！快去找许增威！");
                               });
-                              //console.log(dataForProfitUsing);
-                              this.setState({ filteredData: data }, () =>
-                                resolve()
-                              );
-                            })
-                            .catch(() => {
-                              alert("出错啦！快去找许增威！");
-                            });
-                        });
-                    } else {
-                      axios
-                        .patch(
-                          `https://sneaker-system.herokuapp.com/sneakers/${id}`,
-                          newData
-                        )
-                        .then(() => {
-                          const { buy_price, sold_price } = newData;
-                          const newProfit = sold_price - buy_price;
-                          newData = {
-                            ...newData,
-                            profit: newProfit,
-                          };
-
-                          const { id: newDataId } = newData;
-                          data = data.map((sneaker) => {
-                            const { id } = sneaker;
-                            if (id == newDataId) {
-                              return (sneaker = newData);
-                            } else {
-                              return sneaker;
-                            }
                           });
-                          //console.log(dataForProfitUsing);
-                          this.setState({ filteredData: data }, () =>
-                            resolve()
-                          );
-                        })
-                        .catch(() => {
-                          alert("出错啦！快去找许增威！");
-                        });
-                    }
-                  }
-                  resolve();
-                }, 1000);
-              }),
-            onRowDelete: (oldData) =>
-              new Promise((resolve, reject) => {
-                setTimeout(() => {
-                  {
-                    const data = Array.from(this.state.filteredData);
-                    const index = data.indexOf(oldData);
-                    data.splice(index, 1);
-                    const { id } = oldData;
-                    if (!id) {
-                      fetch("https://sneaker-system.herokuapp.com/sneakers")
-                        .then((res) => {
-                          return res.json();
-                        })
-                        .then((sneakers) => {
-                          const indexWithId = sneakers.indexOf(oldData);
-                          const dataWithId = sneakers.splice(indexWithId, 1);
+                      } else {
+                        axios
+                          .patch(
+                            `https://sneaker-system.herokuapp.com/sneakers/${id}`,
+                            newData
+                          )
+                          .then(() => {
+                            const { buy_price, sold_price } = newData;
+                            const newProfit = sold_price - buy_price;
+                            newData = {
+                              ...newData,
+                              profit: newProfit,
+                            };
 
-                          const { id } = dataWithId[0];
-
-                          axios
-                            .delete(
-                              `https://sneaker-system.herokuapp.com/sneakers/${id}`
-                            )
-                            .then(() => {
-                              this.setState({ filteredData: data }, () =>
-                                resolve()
-                              );
-                            })
-                            .catch(() => {
-                              alert("出错啦！快去找许增威！");
+                            const { id: newDataId } = newData;
+                            data = data.map((sneaker) => {
+                              const { id } = sneaker;
+                              if (id == newDataId) {
+                                return (sneaker = newData);
+                              } else {
+                                return sneaker;
+                              }
                             });
-                        });
-                    } else {
-                      axios
-                        .delete(
-                          `https://sneaker-system.herokuapp.com/sneakers/${id}`
-                        )
-                        .then(() => {
-                          this.setState({ filteredData: data }, () =>
-                            resolve()
-                          );
-                        })
-                        .catch(() => {
-                          alert("出错啦！快去找许增威！");
-                        });
+                            //console.log(dataForProfitUsing);
+                            this.setState({ filteredData: data }, () =>
+                              resolve()
+                            );
+                          })
+                          .catch(() => {
+                            alert("出错啦！快去找许增威！");
+                          });
+                      }
                     }
-                  }
-                  resolve();
-                }, 1000);
-              }),
-          }}
-        />
+                    resolve();
+                  }, 1000);
+                }),
+              onRowDelete: (oldData) =>
+                new Promise((resolve, reject) => {
+                  setTimeout(() => {
+                    {
+                      const data = Array.from(this.state.filteredData);
+                      const index = data.indexOf(oldData);
+                      data.splice(index, 1);
+                      const { id } = oldData;
+                      if (!id) {
+                        fetch("https://sneaker-system.herokuapp.com/sneakers")
+                          .then((res) => {
+                            return res.json();
+                          })
+                          .then((sneakers) => {
+                            const indexWithId = sneakers.indexOf(oldData);
+                            const dataWithId = sneakers.splice(indexWithId, 1);
+
+                            const { id } = dataWithId[0];
+
+                            axios
+                              .delete(
+                                `https://sneaker-system.herokuapp.com/sneakers/${id}`
+                              )
+                              .then(() => {
+                                this.setState({ filteredData: data }, () =>
+                                  resolve()
+                                );
+                              })
+                              .catch(() => {
+                                alert("出错啦！快去找许增威！");
+                              });
+                          });
+                      } else {
+                        axios
+                          .delete(
+                            `https://sneaker-system.herokuapp.com/sneakers/${id}`
+                          )
+                          .then(() => {
+                            this.setState({ filteredData: data }, () =>
+                              resolve()
+                            );
+                          })
+                          .catch(() => {
+                            alert("出错啦！快去找许增威！");
+                          });
+                      }
+                    }
+                    resolve();
+                  }, 1000);
+                }),
+            }}
+          />
+        </div>
       </div>
     );
   }
